a67b8d18c911c6a29b16bc81355a180ec386025f
Jordan Cook <jcook2395@gmail.com>
2015-09-25 15:32:33 -0400

    started on directives

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@ var app = angular.module('CirklarnaApp', []);
 app.controller('HomeController', ['$scope', function(scope) {
 
 }]);
+
+app.directive('myHome', function() {
+	
+	return {
+		restrict: 'E',
+		templateUrl: 'my-home.html'
+	};
+
+});
+
+app.directive('myProps', function() {
+
+	return {
+		restrict: 'E',
+		templateUrl: 'my-props.html'
+	};
+
+});
