7ca842e76b8a1b360b610ea31f605cc9fbe8406e
jordan <jcook2395@gmail.com>
2015-10-08 23:42:19 -0400

    using as a backup save

--- js/app.js
+++ js/app.js
@@ -7,4 +7,48 @@ app.controller('HomeController', ['$scope', function($scope) {
 
 }]);
 
+app.directive('homePage', function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/html/home-page.html'
+	};
+});
+
+app.controller('PropsController', ['$scope', '$http', '$q', 'propsService', function($scope, $http, $q, propsService) {
+	$scope.loadProps = function() {
+		propsService.loadProps().
+		then(function(propsData) {
+			$scope.propsData = propsData;
+		});
+	};
+}]);
+
+app.directive('propPage', function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/html/prop-page.html',
+		controllerAs: 'pc',
+		controller: 'PropsController'
+	};
+});
+
+app.factory('propsService', ['$scope', '$http', '$q', function($scope, $http, $q) {
+	return {
+		loadProps: function() {
+			var deferred = $q.defer();
+			$http.get('/json/props.json')
+				.success(function(data){
+					deferred.resolve({
+						propName: data.propName,
+						description: data.description,
+						performers: data.performers});
+				}).error(function(msg, code) {
+					deferred.reject(msg);
+					$log.error(msg, code);
+					});
+				return deferred.promise;
+		}
+	};
+}]);
+
 
